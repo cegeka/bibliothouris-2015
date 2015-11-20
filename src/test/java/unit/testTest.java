ae7abaff3cdac9c9be5b4b95e156ebd8a71d31f7
George <george.sandu.grigorescu@gmail.com>
2015-11-20 15:56:23 +0200

    added test test for testing the tests so that we can test while we test.

---
+++ src/test/java/unit/testTest.java
@@ -0,0 +1,12 @@
+package unit;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class testTest {
+
+    @Test
+    public void testingTheTests(){
+        Assertions.assertThat(Boolean.TRUE).isEqualTo(Boolean.TRUE);
+    }
+}
