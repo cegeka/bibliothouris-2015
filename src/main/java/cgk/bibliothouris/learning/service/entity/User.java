0bbf41e733681cb992d00d7f8b7950a65acc39d4
Iulia Bivolaru <iulia.bivolaru@cegeka.com>
2015-11-23 15:18:15 +0200

    User entity creation

---
+++ src/main/java/cgk/bibliothouris/learning/service/entity/User.java
@@ -0,0 +1,43 @@
+package cgk.bibliothouris.learning.service.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @SequenceGenerator(name = "book_user_sequence", sequenceName = "BOOK_USER_SEQUENCE", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_user_sequence")
+    @Column(name = "USER_ID")
+    private Integer id;
+
+    @Column(name = "USERNAME")
+    private String name;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
