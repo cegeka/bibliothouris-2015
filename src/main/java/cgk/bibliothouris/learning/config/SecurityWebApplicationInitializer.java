01a0ec441aacc54c1d428da0570fd2a810cdafa5
George <george.sandu.grigorescu@gmail.com>
2015-11-24 18:47:42 +0200

    add first iteration of spring security to the project.

---
+++ src/main/java/cgk/bibliothouris/learning/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package cgk.bibliothouris.learning.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+
+}
