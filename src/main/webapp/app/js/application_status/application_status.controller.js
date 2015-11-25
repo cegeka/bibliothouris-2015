7379afd78fda54c07b58d8fa534f195b0fa883d6
George <george.sandu.grigorescu@gmail.com>
2015-11-25 18:30:08 +0200

    Moved stuff from one place to another.

--- src/main/webapp/app/js/application_status/application_status.controller.js
+++ src/main/webapp/app/js/application_status/application_status.controller.js
@@ -4,20 +4,16 @@
         .controller("ApplicationStatusController", ApplicationStatusController);
 
     function ApplicationStatusController(applicationStatusService) {
-        console.log("controller");
-
         var vm = this;
-
         vm.status = {};
-
         activate();
-
         function activate() {
             applicationStatusService
                 .getStatus()
                 .then(function(data){
                     vm.status = data;
                 });
-        }
+        };
+        console.log("controller");
     }
 })();
