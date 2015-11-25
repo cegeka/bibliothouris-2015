12ca1fa747852588019691b33032255b8df61ec5
valentinabojan <valentina.bojan@cti.pub.ro>
2015-11-25 16:31:40 +0200

    Add Angular service for REST API access

---
+++ src/main/webapp/js/service/rest_api.service.js
@@ -0,0 +1,20 @@
+(function() {
+    angular
+        .module("Bibliothouris")
+        .factory("restService", restService);
+
+    function restService($http) {
+        var service = {
+            addBook: addBook
+        };
+
+        return service;
+
+        function addBook() {
+            return $http.post("/api/books", book)
+                .then(function(response){
+                    return response.data;
+                });
+        }
+    }
+})();
