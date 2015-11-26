faa7d1bf9e1257b4b3a8ddcefc3b8ac452f357c9
valentinabojan <valentina.bojan@cti.pub.ro>
2015-11-26 12:20:24 +0200

    Add informative popup when a book was successfully added

--- src/main/webapp/app/js/add_book/add_book.controller.js
+++ src/main/webapp/app/js/add_book/add_book.controller.js
@@ -38,20 +38,39 @@
                 .addBook(vm.book)
                 .then(function(data){
                     $location.path("/books/" + data.id);
+                    createNotification("Book <strong>" + data.title + "</strong> was added in the library!", "success")
+                }, function(data){
+                    createNotification("Something wrong happened when you tried to add a new book!", "danger")
                 });
-
-            $uibModal.open({
-                templateUrl: 'templates/informative_modal.html',
-                resolve: {
-                    message: function () {
-                        return "ceva de test";
-                    }
-                }
-            });
         }
 
         function resetForm() {
             vm.book = angular.copy(vm.originalBook);
+        }
+
+        function createNotification(message, type) {
+            $.notify(message,{
+                placement: {
+                    from: "bottom",
+                    align: "right"
+                },
+                type: type,
+                delay: 1500,
+                animate: {
+                    enter: 'animated fadeInUp',
+                    exit: 'animated fadeOutDown'
+                },
+                template: '<div data-notify="container" class="col-xs-11 col-sm-2 alert alert-{0}" role="alert">' +
+                '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                '<span data-notify="icon"></span> ' +
+                '<span data-notify="title">{1}</span> ' +
+                '<span data-notify="message">{2}</span>' +
+                '<div class="progress" data-notify="progressbar">' +
+                '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+                '</div>' +
+                '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                '</div>'
+            });
         }
     }
 })();
