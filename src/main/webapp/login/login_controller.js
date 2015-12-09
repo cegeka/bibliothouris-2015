555fdc9371741a72d102f2a2718b804b12b6ff0b
George <george.sandu.grigorescu@gmail.com>
2015-12-09 19:02:53 +0200

    added fade to the logout and login error messages.

--- src/main/webapp/login/login_controller.js
+++ src/main/webapp/login/login_controller.js
@@ -3,16 +3,37 @@
         .module("Login")
         .controller("LoginCtrl", LoginCtrl);
 
-    function LoginCtrl($scope, $location) {
+    function LoginCtrl($scope, $location, $timeout) {
+
+        $scope.hideTimeout = 5000;
+        $scope.urlChangeTimeout = $scope.hideTimeout + 2100;
+
+        $scope.hideStuff = function () {
+            $scope.startFade = true;
+        };
 
         $scope.loginFailed = function () {
-            if (($location.search()).error) {
-                return "has-error";
-            }
+            $timeout($scope.hideStuff, $scope.hideTimeout);
+            $timeout($scope.setUrl, $scope.urlChangeTimeout);
+            return $scope.getloginFailedStatus();
         }
 
         $scope.logoutSuccess = function () {
+            $timeout($scope.hideStuff, $scope.hideTimeout);
+            $timeout($scope.setUrl, $scope.urlChangeTimeout);
+            return $scope.getLogoutStatus();
+        }
+
+        $scope.setUrl = function () {
+            $location.url($location.path())
+        }
+
+        $scope.getLogoutStatus = function () {
             return ($location.search()).logout;
+        }
+
+        $scope.getloginFailedStatus = function () {
+            return ($location.search()).error;
         }
     }
 })();
