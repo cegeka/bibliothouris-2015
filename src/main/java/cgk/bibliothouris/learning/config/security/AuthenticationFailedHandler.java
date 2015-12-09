854e4bad32791e0d9954f6469aaa8361ef21a89d
George <george.sandu.grigorescu@gmail.com>
2015-12-09 15:32:10 +0200

    update authFailedhandler so that we have the # sign in the link so we can use $location

--- src/main/java/cgk/bibliothouris/learning/config/security/AuthenticationFailedHandler.java
+++ src/main/java/cgk/bibliothouris/learning/config/security/AuthenticationFailedHandler.java
@@ -14,6 +14,6 @@ public class AuthenticationFailedHandler extends SimpleUrlAuthenticationFailureH
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        getRedirectStrategy().sendRedirect(request, response, "/login.html?error");
+        getRedirectStrategy().sendRedirect(request, response, "/login.html#?error");
     }
 }
